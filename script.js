fff82217b06e9e69bab53d203a0f0e1f37d2e5f7
Phlipper <marzullop73@gmail.com>
2021-02-06 20:24:40 -0500

    add function to create a list of input cities

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$(document).ready(function() {
+function createCityList(citySearchList) {
+    $("#city-list").empty();
+    var keys = Object.keys(citySearchList);
+    for (var i = 0; i < keys.length; i++) {
+      var cityListEntry = $("<button>");
+      cityListEntry.addClass("list-group-item list-group-item-action");
